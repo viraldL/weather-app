ab3e15ae1acae492cae2140eb75d02f1e868abff
viraldL <sopel.sob19@wp.pl>
2023-04-13 18:16:26 +0200

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  performance: {
+    hints: false,
+  },
+};
