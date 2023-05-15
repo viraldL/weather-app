1384a6fea6d874b71698f6144e6721177dd1dc89
viraldL <sopel.sob19@wp.pl>
2023-05-15 16:49:11 +0200

    Finish hourly temp info

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import requestWeather from "./request.js";
-// console.log(requestWeather("Wroclaw").then((res) => {console.log(res.location.name)}));
+// console.log(requestWeather("Wroclaw").then((res) => {console.log(res)}));
