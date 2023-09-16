22123152a9118be26f3332e834d5cdd84dcf8834
viraldL <107942715+viraldL@users.noreply.github.com>
2023-09-16 02:04:28 +0200

    Change request to HTTPS

--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 export default async function requestWeather(req) {
     try {
-        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=8f7668839d52444a8df183310230604&q=${req}&days=5&aqi=no&alerts=no`);
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=8f7668839d52444a8df183310230604&q=${req}&days=5&aqi=no&alerts=no`);
         const weatherData = await response.json();
         return weatherData;
     } catch(err) {
